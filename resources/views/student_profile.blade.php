395cd4991407416eba2ed19bddb4efcbc90b178a
Mohammad s. Albay <43953576+MohammadAlbay@users.noreply.github.com>
2023-03-25 03:33:48 +0200

    first commit

---
+++ resources/views/student_profile.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: rgb(231, 231, 231);
+            overflow: hidden;
+        }
+
+        .main {
+            margin: 0 auto;
+            width: 400px; height: 450px;
+            background-color: rgb(243, 243, 243);
+            box-shadow: #dbdbdb 0px 0px 10px 2px;
+            border-radius: 0.7em;
+            margin-top: 140px;
+        }
+
+        h3.title {text-align: center;}
+
+        tr > td {
+            background-color: #dbdbdb;
+            border-radius: 0.3em;
+        }
+        tr > td:first-child {
+            text-decoration: solid;
+            font-size: 13pt;
+            padding-left: 20px;
+        }
+        tr > td:last-child {
+            font-size: 11.5pt;
+            
+        }
+    </style>
+</head>
+<body>
+    <div class="main">
+        <br />
+        <h3 class="title">البيانات الشخصية للطالبة وجدان  </h3>
+        <table style="margin: 0 auto;" dir="rtl">
+            <tr>
+                <td>الاسم</td>
+                <td>وجدان منير الصغير</td>
+            </tr>
+            <tr>
+                <td>تاريخ الميلاد</td>
+                <td>24/1/2000</td>
+            </tr>
+            <tr>
+                <td>البريد الإلكتروني</td>
+                <td>wijdan.moner@gmail.com</td>
+            </tr>
+            <tr>
+                <td>الكلية</td>
+                <td>كلية تقنية المعلومات</td>
+            </tr>
+            <tr>
+                <td>التخصص</td>
+                <td>هندسة برمجيات - SE</td>
+            </tr>
+
+        </table>
+    </div>
+</body>
+</html>
